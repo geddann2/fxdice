48489d7c00ab374c2441f369b6c976f2c764b2fa
geddann2 <geddann2@gmail.com>
2025-03-14 11:43:29 +0900

    intelJidea to commit

--- src/main/java/com/example/fxdice/Dice.java
+++ src/main/java/com/example/fxdice/Dice.java
@@ -13,7 +13,8 @@ public class Dice {
         Random rand = new Random();
         this.number = rand.nextInt(6) + 1; //1～6
     }
-    //１～６までの数値を返すようにするyoaaa
+    //１～６までの数値を返すようにするyoaaaa
+
     public int getNumber(){
         return this.number;
     }
